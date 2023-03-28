7a1181e6e584371d09caadb1967f57535d38f381
motlalepulag <motlalepulag.phiri@gmail.com>
2023-03-28 13:59:40 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -85,22 +85,10 @@ int _printf(const char *format, ...)
 		}
 
 	}
-
-
-
 	print_buffer(buffer, &buff_ind);
-
-
-
 	va_end(list);
-
-
-
 	return (printed_chars);
-
 }
-
-
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
@@ -109,15 +97,9 @@ int _printf(const char *format, ...)
  */
 
 void print_buffer(char buffer[], int *buff_ind)
-
 {
-
 	if (*buff_ind > 0)
-
 		write(1, &buffer[0], *buff_ind);
-
-
-
 	*buff_ind = 0;
 
 }
